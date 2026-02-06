9977ca1edd7da2ab6bcdc13ab5227d1d5ed95051
farjadp <farjad@live.com>
2026-02-06 14:12:39 -0500

    fix(middleware): update matcher to support non-localized routes

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,10 @@ import { routing } from './routing';
 export default createMiddleware(routing);
 
 export const config = {
-    // Match only internationalized pathnames
-    matcher: ['/', '/(fa|en)/:path*']
+    // Match all pathnames except for:
+    // - /api routes
+    // - /_next (Next.js internals)
+    // - /_vercel (Vercel internals)
+    // - static files (paths with dots)
+    matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
 };
